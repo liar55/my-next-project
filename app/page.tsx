ef323a16fda8f9d984664eb084275b9d295e2b60
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-11-15 15:44:18 +0900

    sliceDataを使用して最新のニュース記事を表示するように変更しました

--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,7 @@ const data:{contents: News[]} = {
 };
 
 export default function Home() {
+  const sliceData = data.contents.slice(0,2);
   return (
     <>
     <section className={styles.top}>
@@ -62,7 +63,7 @@ export default function Home() {
     <section className={styles.news}>
       <h2 className={styles.newsTitle}>News</h2>
       <ul>
-        {data.contents.map((article) => (
+        {sliceData.map((article) => (
           <li key={article.id} className={styles.list}>
             <div className={styles.link}>
               <Image
