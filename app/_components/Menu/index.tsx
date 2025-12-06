3a6684b14a3195b622d451156e553141b98e5fc9
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-12-06 15:36:04 +0900

    Menuコンポーネントのスタイルを更新し、ナビゲーションの表示方法を改善しました。また、classnamesパッケージを追加しました。

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import styles from "./index.module.css";
 
 export default function Menu() {
+    const open = () => {
+        document.querySelector("nav")?.classList.add(styles.open);  
+    };
     return (
         <div>
         <nav className={styles.nav}>
@@ -18,7 +21,7 @@ export default function Menu() {
                 </li>
             </ul>
         </nav>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={open}>
           <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
         </button>
       </div>
