28769e7be0b291d552d26a7f55f854c3e9b71036
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-12-13 14:51:04 +0900

    ニュースリストコンポーネントを更新し、JSON表示からNewsListコンポーネントを使用するように変更しました。また、dayjsライブラリを追加し、日付フォーマット用のユーティリティ関数を作成しました。

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,8 @@
 import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
 
 export default async function Page() {
     const { contents:news } = await getNewsList();
 
-    return <div>{JSON.stringify(news)} </div>;
+    return <NewsList news={news} />;
 }
