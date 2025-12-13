aca459c0135dca33cb945716bba24e1a924ed904
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-12-13 16:06:50 +0900

    ボタンリンクコンポーネントを新規作成し、スタイルを追加しました

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,37 @@ import Image from "next/image";
 import { getNewsList } from "@/app/_libs/microcms";
 import { TOP_NEWS_LIST_LIMIT } from "@/app/_constans";
 import NewsList from "@/app/_components/NewsList";
-import ButtonLink from "@/app/_components/ButtonLink";
+import ButtonLink from "@/app/_components/_ButtonLink";
 
 export default async function Home() {
   const data = await getNewsList({ limit: TOP_NEWS_LIST_LIMIT });
- 
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-    <section className={styles.top}>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+        <section className={styles.top}>
+          <div>
+            <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+            <p className={styles.description}>
+              私たちは市場をリードしているグローバルテックカンパニーです。
+            </p>
+          </div>
+          <Image
+            className={styles.bgimg}
+            src="/img-mv.jpg"
+            alt=""
+            width={4000}
+            height={1200}
+          />
+        </section>
+        <section className={styles.news}>
+          <h2 className={styles.newsTitle}>News</h2>
+          <NewsList news={data.contents} />
+          <div className={styles.newsLink}>
+            <ButtonLink href="/news">もっと見る</ButtonLink>
+          </div>
+        </section>
       </div>
-      <Image
-        className={styles.bgimg} 
-        src="/img-mv.jpg" 
-        alt=""
-        width={4000}
-        height={1200}
-        />
-    </section>
-    <section className={styles.news}>
-      <h2 className={styles.newsTitle}>News</h2>
-      <NewsList news={data.contents} />
-        <div className={styles.newsLink}>
-          <ButtonLink href="/news">もっと見る</ButtonLink>
-        </div>
-      </section>
-      </div>   
     </main>
-  );  
+  );
 }
