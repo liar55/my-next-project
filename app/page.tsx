aebc049a5e93e0b72476db533479e0dff49655de
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-12-20 14:47:11 +0900

    revalidate定数を各ページコンポーネントに追加

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { TOP_NEWS_LIST_LIMIT } from "@/app/_constans";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const data = await getNewsList({ limit: TOP_NEWS_LIST_LIMIT });
 
