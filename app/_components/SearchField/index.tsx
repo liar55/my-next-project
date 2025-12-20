ba8df7f415280952e545b502fedcbbbd89b84abd
noa <KTC25A31E0017@edu.kyoto-tech.ac.jp>
2025-12-20 14:24:41 +0900

    コードのインデントを修正し、可読性を向上させました。

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,44 +1,44 @@
-"use client";   
+"use client";
 
 import Image from "next/image";
-import { useRouter,useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import styles from "./index.module.css";
 import { Suspense } from "react";
 
-function SearchFieldComponent () {
-    const router = useRouter();
-    const searchParams = useSearchParams();
+function SearchFieldComponent() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const q = e.currentTarget.elements.namedItem("q");
-        if (q instanceof HTMLInputElement) {
-            const params = new URLSearchParams();
-            params.set("q",q.value.trim());
-            router.push(`/news/search?${params.toString()}`);
-        }
-    };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const q = e.currentTarget.elements.namedItem("q");
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      params.set("q", q.value.trim());
+      router.push(`/news/search?${params.toString()}`);
+    }
+  };
 
-
-    return (
-        <form onSubmit={handleSubmit} className={styles.form}>
-            <label className ={styles.label}>
-                <Image src="/serch.svg" alt="検索" width={16} height={16} />
-                <input 
-                  type ="text"
-                  name ="q"
-                  defaultValue={searchParams.get("q") ?? undefined}
-                  placeholder="キーワードを入力"
-                  className={styles.serchInput}
-                />
-            </label>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label className={styles.label}>
+        <Image src="/serch.svg" alt="検索" width={16} height={16} />
+        <input
+          type="text"
+          name="q"
+          defaultValue={searchParams.get("q") ?? undefined}
+          placeholder="キーワードを入力"
+          className={styles.serchInput}
+        />
+      </label>
+    </form>
+  );
 }
 
 export default function SearchField() {
-    return (
-        <Suspense>
-          <SearchFieldComponent />
-        </Suspense>
-    );
+  return (
+    <Suspense>
+      <SearchFieldComponent />
+    </Suspense>
+  );
 }
